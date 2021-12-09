190d2c7bf89aa5f5cb34ba7d8f56c07042500d0b
rennan abreu <amrennan@gmail.com>
2021-12-09 14:31:49 -0300

    Exercícios de laços de desisão

---
+++ Introducao/src/Condicionais/Exercicio_4.java
@@ -0,0 +1,35 @@
+package Condicionais;
+
+import java.util.Scanner;
+
+public class Exercicio_4 {
+	
+	public static void main(String[] args) {
+	
+		Scanner in = new Scanner(System.in);
+		float num=0, rest=0, x=0;
+		boolean teste;
+	
+		System.out.print("Digite um número: ");
+		num=in.nextFloat();
+		
+		rest=num%2;
+		teste=rest==0;
+		
+		if (teste)
+		{
+			x= (float) Math.sqrt(num);
+			System.out.println("O número digitado é par e sua raiz quadrada é "+x);
+		}
+		else
+		{
+			x= (float) Math.pow(num,2);
+			System.out.println("O número digitado é impar e o mesmo elevado ao quadrado é "+x);
+		}
+		
+	
+	
+	}
+	
+
+}
