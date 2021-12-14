78c956e3e7847c0bd828228d9bf8deb068e6a9b0
rennan abreu <amrennan@gmail.com>
2021-12-14 16:25:59 -0300

    Execícios de Introdução

---
+++ Introducao/src/IntroducaoPoo/Aviao.java
@@ -0,0 +1,49 @@
+package IntroducaoPoo;
+
+public class Aviao {
+	
+	private String cor, modelo, companhia;
+	private boolean ligado, embarque;
+	
+	public Aviao() {
+		
+		this.ligado=false;
+		this.embarque=false;
+	}
+	
+	
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public void setCompanhia(String companhia) {
+		this.companhia = companhia;
+	}
+	
+	public void liga() {
+		this.ligado=true;
+	}
+	
+	public void passageirosAbordo() {
+		this.embarque=true;
+	}
+
+	public void decolar() {
+		if(ligado&&embarque)
+		{
+			System.out.println("O avião esta decolando!");
+		}
+		else
+		{
+			System.out.println("ERRO: O avião não pode decolar");
+		}
+	}
+	
+	public void status() {
+		System.out.println("O avião é da cor: "+this.cor);
+		System.out.println("O voô será "+this.modelo);
+		System.out.println("A companhia escolhida é "+this.companhia);
+	}
+}
---
+++ Introducao/src/IntroducaoPoo/Eletronico.java
@@ -0,0 +1,74 @@
+package IntroducaoPoo;
+
+public class Eletronico {
+	
+	private String tipo, nome, sistema, processador;
+	private int memoriaRam, ssd, hd, placaVideo;
+	private boolean comprar;
+	
+	public Eletronico() {
+		
+		this.sistema="Windows";
+		this.processador="Intel core i7";
+		this.memoriaRam=4;
+		this.ssd=512;
+		this.hd=2;
+		this.placaVideo=4;
+		this.comprar=false;
+		
+		
+	}
+	
+	public String getSistema() {
+		return sistema;
+	}
+	public String getProcessador() {
+		return processador;
+	}
+	public int getMemoriaRam() {
+		return memoriaRam;
+	}
+	public int getSsd() {
+		return ssd;
+	}
+	public int getHd() {
+		return hd;
+	}
+	public int getPlacaVideo() {
+		return placaVideo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setComprar(boolean comprar) {
+		this.comprar = comprar;
+	}
+
+	public void status() {
+		
+		if(comprar)
+		{
+			System.out.println("O "+this.tipo+" foi comprado!");
+		}
+		else
+		{
+			System.out.println("O produto foi não comprado!");
+		}
+		
+	}
+	
+	
+	
+	
+	
+
+}
